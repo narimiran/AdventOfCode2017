8d1e391ea26e2d85d0537a8dbde0016f7227b4e0
miran.tuhtan <miran.tuhtan@uniri.hr>
2018-03-29 10:48:12 +0200

    faster day06 in Nim
    Seqs can be used in sets, no need to convert them to strings.

--- nim/day06.nim
+++ nim/day06.nim
@@ -2,24 +2,23 @@ import strutils, sequtils, sets
 
 var
   banks = readFile("./inputs/06.txt").split.map(parseInt)
-  size = len(banks)
-  seen = initOrderedSet[string]()
+  seen = initOrderedSet[seq[int]]()
+  maxValue, maxIndex: int
 
-seen.incl(banks.join(" "))
+seen.incl(banks)
 
 while true:
-  let
-    maxValue = max(banks)
-    maxIndex = banks.find(maxValue)
+  maxValue = max(banks)
+  maxIndex = banks.find(maxValue)
 
   banks[maxIndex] = 0
-  for i in 1 .. maxValue: inc banks[(maxIndex + i) mod size]
+  for i in 1 .. maxValue:
+    inc banks[(maxIndex + i) and 15]
 
-  let banksHash = banks.join(" ")
-  if banksHash notin seen: seen.incl(banksHash)
+  if banks notin seen: seen.incl(banks)
   else: break
 
-let loopStart = toSeq(seen.items).find(banks.join(" "))
+let loopStart = toSeq(seen.items).find(banks)
 
 echo len(seen)
-echo len(seen) - loopStart 
+echo len(seen) - loopStart
