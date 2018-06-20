08aae3df18a8d1c10b1c435c86905afc8df7dbc3
narimiran <narimiran@disroot.org>
2018-06-20 15:32:31 +0200

    cosmetic changes in Nim01

--- nim/day01.nim
+++ nim/day01.nim
@@ -1,14 +1,16 @@
-var numbers: seq[int] = @[]
-for i in readFile("./inputs/01.txt"):
-  numbers.add(int(i) - int('0'))
+proc readDigits(f: string): seq[int] =
+  for i in readFile(f):
+    result.add(int(i) - int('0'))
 
-proc solve(numbers: seq[int], secondPart=false): int =
-  let
-    size = len(numbers)
-    jump = if secondPart: size div 2 else: 1
-  for i, n in numbers:
-    if n == numbers[(i + jump) mod size]:
+const
+  digits = readDigits("./inputs/01.txt")
+  size = digits.len
+
+
+func solve(jump: int): int =
+  for i, n in digits:
+    if n == digits[(i + jump) mod size]:
       result += n
-      
-echo solve(numbers)
-echo solve(numbers, secondPart=true)
+
+echo solve(1)
+echo solve(size div 2)
