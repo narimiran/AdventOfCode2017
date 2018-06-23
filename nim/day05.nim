b83dffdbe7d2ddc402824db5657f500caf0830fe
narimiran <narimiran@disroot.org>
2018-06-23 15:55:52 +0200

    clearer Nim05

--- nim/day05.nim
+++ nim/day05.nim
@@ -1,25 +1,22 @@
-import strutils
+import strutils, sequtils
 
-const input = readFile("./inputs/05.txt").splitLines
-type Instructions = array[input.len, int]
-var instructions: Instructions
-for i, line in input: instructions[i] = line.parseInt
+const instructions = readFile("./inputs/05.txt").splitLines.map(parseInt)
 
 
-proc stepByStep(originalInstructions: Instructions, secondPart = false): uint =
+func stepByStep(originalInstructions: seq[int], secondPart = false): uint32 =
   var
     instructions = originalInstructions
-    line: int
+    line, jump: int
+
   while line < instructions.len:
-    let jump = instructions[line]
-    if secondPart and jump >= 3: dec instructions[line]
-    else: inc instructions[line]
+    jump = instructions[line]
+    if secondPart and jump >= 3:
+      dec instructions[line]
+    else:
+      inc instructions[line]
     line += jump
     inc result
 
 
-let first = instructions.stepByStep()
-let second = instructions.stepByStep(secondPart=true)
-
-echo first
-echo second
+echo instructions.stepByStep()
+echo instructions.stepByStep(secondPart=true)
