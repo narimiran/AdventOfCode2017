cfaedc078b46e9d563328e962002ffc7919351aa
narimiran <narimiran@disroot.org>
2018-06-24 16:11:35 +0200

    cleaner Nim10
    * use `swap` instead of manually swapping the elements
    * use `map` instead of list comprehensions
    * use `func` (Nim v0.18.1 feature) to denote functios without side
    effects

--- nim/day10.nim
+++ nim/day10.nim
@@ -1,47 +1,40 @@
-import future, strutils, sequtils, algorithm
-
+import sugar, strutils, sequtils
 
 const res = lc[i | (i <- 0 .. 255), int]
 
-proc knotLogic(sizes: seq[int], iterations = 64): seq[int] =
+
+func knotLogic(sizes: seq[int], iterations = 64): seq[int] =
   result = res
   const size = 255
-  var
-    pos: int
-    skip: int
-    a, b, tmp: int
+  var pos, skip, a, b: int
+
   for i in 1 .. iterations:
     for groupSize in sizes:
       for p in 0 ..< (groupSize div 2):
         a = (pos + p) and size
         b = (pos + groupSize-1 - p) and size
-        tmp = result[b]
-        result[b] = result[a]
-        result[a] = tmp
+        swap(result[a], result[b])
       pos = (pos + groupSize + skip) and size
       inc skip
 
 
-proc knotHashing*(word: string, binOut = false): string =
-  result = ""
-  const toAdd = @[17, 31, 73, 47, 23]
+func knotHashing*(word: string, binOut = false): string =
   let
-    asciiSizes = lc[ord(c) | (c <- word), int].concat(toAdd)
+    asciiSizes = word.mapIt(ord it).concat(@[17, 31, 73, 47, 23])
     numbers = knotLogic(asciiSizes)
   for bl in 0 .. 15:
     var hashed: int
     for i in 0 .. 15:
       hashed = hashed.xor(numbers[16*bl + i])
-    if binOut:
-      result.add(hashed.toBin(8))
-    else:
-      result.add(hashed.toHex(2))
+
+    let hashOut = if binOut: hashed.toBin(8) else: hashed.toHex(2)
+    result.add(hashOut)
 
 
 when isMainModule:
   let
     inputString = readFile("./inputs/10.txt")
-    intSizes = lc[parseInt(n) | (n <- inputString.split(',')), int]
+    intSizes = inputString.split(',').map(parseInt)
     firstHash = knotLogic(intSizes, 1)
     first = firstHash[0] * firstHash[1]
     second = knotHashing(inputString).toLowerAscii()
