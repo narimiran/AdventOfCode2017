66b593fb0edb5c16c91464fdb15df946a66c819f
narimiran <narimiran@disroot.org>
2018-07-11 18:20:37 +0200

    cleaner Nim12

--- nim/day12.nim
+++ nim/day12.nim
@@ -7,26 +7,31 @@ var
   graph = initTable[int, seq[int]]()
   seen = initSet[int]()
   groups: int
+  nodes: seq[string]
+  pipe: int
+  neighbours: seq[int]
+
 
 for line in instructions:
-  let
-    nodes = line.split(" <-> ")
-    pipe = nodes[0].parseInt()
-    neighbours = nodes[1].split(", ").map(parseInt)
+  nodes = line.split(" <-> ")
+  pipe = nodes[0].parseInt()
+  neighbours = nodes[1].split(", ").map(parseInt)
   graph[pipe] = neighbours
 
 
 proc dfs(startingPoint: int): HashSet[int] =
   result = initSet[int]()
-  var stack = @[startingPoint]
+  var
+    stack = @[startingPoint]
+    current: int
   while stack.len > 0:
-    let current = stack.pop()
+    current = stack.pop()
     result.incl(current)
     for node in graph[current]:
       if node notin result:
         stack.add(node)
 
-proc `+=`(a: var HashSet, b: HashSet) = a = a + b
+func `+=`(a: var HashSet, b: HashSet) = a = a + b
 
 
 let firstIsland = dfs(start)
