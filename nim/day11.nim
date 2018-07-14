6b53b484ac70edc29caca7421647bd7b6ddab5ea
narimiran <narimiran@disroot.org>
2018-07-14 17:20:00 +0200

    cleaner Nim11

--- nim/day11.nim
+++ nim/day11.nim
@@ -6,22 +6,20 @@ type Point = tuple[p: int, q: int]
 
 func toAxial(direction: string): Point =
   case direction
-  of "n": return (0, -1)
-  of "nw": return (-1, 0)
-  of "sw": return (-1, 1)
-  of "s": return (0, 1)
-  of "se": return (1, 0)
-  of "ne": return (1, -1)
+    of "n":  ( 0, -1)
+    of "nw": (-1,  0)
+    of "sw": (-1,  1)
+    of "s":  ( 0,  1)
+    of "se": ( 1,  0)
+    else:    ( 1, -1)
 
 func `+`(a, b: Point): Point = (a.p + b.p, a.q + b.q)
 func `+=`(a: var Point, b: Point) = a = a + b
-
-func distanceToOrigin(a: Point): int =
-  max([abs(a.p), abs(a.q), abs(a.p + a.q)])
+func distanceToOrigin(a: Point): int = max([abs(a.p), abs(a.q), abs(a.p + a.q)])
 
 
 var
-  position: Point = (0, 0)
+  position: Point
   distances: seq[int] = @[]
 
 for direction in instructions:
