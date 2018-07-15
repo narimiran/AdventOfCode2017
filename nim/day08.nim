5a3c82dbcf1d58c12e9f003a05a477d41bc5bf96
narimiran <narimiran@disroot.org>
2018-07-15 12:12:05 +0200

    cleaner Nim08

--- nim/day08.nim
+++ nim/day08.nim
@@ -1,52 +1,42 @@
 import tables, strutils
 
 const instructions = readFile("./inputs/08.txt").splitLines()
-var
-  registers = initTable[string, int]()
-  maximum: int
-  biggest: int
 
-
-proc change(reg, op: string, amount: int) =
-  case op
-  of "inc": registers[reg] += amount
-  of "dec": registers[reg] -= amount
-
-proc checkCondition(reg, op: string, value: int): bool =
-  let regVal = registers[reg]
-  case op
-  of "<": return regVal < value
-  of ">": return regVal > value
-  of "<=": return regVal <= value
-  of ">=": return regVal >= value
-  of "==": return regVal == value
-  of "!=": return regVal != value
-
-
-var
-  words: seq[string]
-  register, instruction: string
-  conditionRegister, conditionOperator: string
+type Instruction = tuple
+  reg, instr, condReg, condOp: string
   amount, value: int
 
 
+func parseLine(w: seq[string]): Instruction =
+  (w[0], w[1], w[^3], w[^2], w[2].parseInt, w[^1].parseInt)
+
+func isConditionSatisfied(op: string, regVal, value: int): bool =
+  case op
+    of "<":  regVal < value
+    of ">":  regVal > value
+    of "<=": regVal <= value
+    of ">=": regVal >= value
+    of "==": regVal == value
+    else:    regVal != value
+
+
+var
+  registers = initCountTable[string]()
+  maximum: int
+
 for line in instructions:
-  words = line.split()
-  register = words[0]
-  instruction = words[1]
-  amount = words[2].parseInt()
-  conditionRegister = words[^3]
-  conditionOperator = words[^2]
-  value = words[^1].parseInt()
-  if not registers.hasKey(register): registers[register] = 0
-  if not registers.hasKey(conditionRegister): registers[conditionRegister] = 0
+  let
+    l = line.split().parseLine()
+    regVal = registers.getOrDefault(l.condReg)
 
-  if checkCondition(conditionRegister, conditionOperator, value):
-    change(register, instruction, amount)
-    if registers[register] > maximum: maximum = registers[register]
+  if isConditionSatisfied(l.condOp, regVal, l.value):
+    case l.instr
+      of "inc":
+        registers.inc(l.reg, l.amount)
+        maximum = max(registers[l.reg], maximum)
+      of "dec":
+        registers.inc(l.reg, -l.amount)
 
-for v in registers.values:
-  if v > biggest: biggest = v
 
-echo biggest
+echo registers.largest.val
 echo maximum
