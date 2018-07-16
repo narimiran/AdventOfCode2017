32621582455a0d52abbf9f2ecf55877962a6c380
narimiran <narimiran@disroot.org>
2018-07-16 20:27:28 +0200

    faster Nim20
    Closures are bottlenecks.
    Don't use them.
    
    Also removed redundand code.

--- nim/day20.nim
+++ nim/day20.nim
@@ -8,7 +8,11 @@ type
 
 func extractNumbers(s: string): Coord =
   let xyz = s[3 .. ^2].split(',').map(parseFloat)
-  result = (xyz[0], xyz[1], xyz[2])
+  (xyz[0], xyz[1], xyz[2])
+
+func parseLine(line: string): Particle =
+  let pva = line.split(", ").map(extractNumbers)
+  (pva[0], pva[1], pva[2])
 
 func manhattan(accel: Coord): float =
   abs(accel.x) + abs(accel.y) + abs(accel.z)
@@ -20,15 +24,12 @@ var
 
 for i, line in instructions:
   let
-    pva = line.split(", ")
-    p = pva[0].extractNumbers
-    v = pva[1].extractNumbers
-    a = pva[2].extractNumbers
-    ma = manhattan(a)
+    particle = line.parseLine()
+    ma = manhattan(particle.a)
   if ma < smallestAcceleration:
     smallestAcceleration = ma
     closestParticle = i
-  particles[i] = (p, v, a)
+  particles[i] = particle
 
 echo closestParticle
 
@@ -38,14 +39,13 @@ echo closestParticle
 func `-`(c1, c2: Coord): Coord = (c1.x - c2.x, c1.y - c2.y, c1.z - c2.z)
 func `-`(p1, p2: Particle): Particle = (p1.p - p2.p, p1.v - p2.v, p1.a - p2.a)
 
-func checkCollision(p, v, a: Coord, time: float): bool =
-  func collision(pp, vv, aa: float): bool =
+func collision(pp, vv, aa, time: float): bool =
     (2 * vv + aa * (1+time)) * time + 2 * pp == 0
-  return collision(p.y, v.y, a.y) and collision(p.z, v.z, a.z)
 
+func checkCollision(p, v, a: Coord, time: float): bool =
+  collision(p.y, v.y, a.y, time) and collision(p.z, v.z, a.z, time)
 
 func findCollisions(p1, p2: Particle): float =
-  result = float.high
   let
     (p, v, a) = p1 - p2
     b = - v.x - 0.5 * a.x
@@ -53,16 +53,16 @@ func findCollisions(p1, p2: Particle): float =
   if a.x == 0:
     if v.x != 0:
       let time = -p.x / v.x
-      if checkCollision(p, v, a, time) and time < result:
-        result = time
+      if checkCollision(p, v, a, time):
+        return time
   elif D == 0:
     let time = b / a.x
-    if checkCollision(p, v, a, time) and time < result:
-      result = time
+    if checkCollision(p, v, a, time):
+      return time
   else:
     for time in [(b - D.sqrt) / a.x, (b + D.sqrt) / a.x]:
-      if checkCollision(p, v, a, time) and time < result:
-        result = time
+      if checkCollision(p, v, a, time):
+        return time
 
 
 var collisions = initTable[int, seq[tuple[i, j: int16]]]()
@@ -75,9 +75,9 @@ for i, p1 in particles:
         collisions[time] = @[]
       collisions[time].add((i.int16, (i+j+1).int16))
 
-var dead: set[int16] = {}
+var dead: set[int16]
 for time in collisions.keys:
-  var colliding: set[int16] = {}
+  var colliding: set[int16]
   for collision in collisions[time]:
     if collision.i notin dead and collision.j notin dead:
       colliding.incl(collision.i)
