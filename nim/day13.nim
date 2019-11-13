33a17b3b516f1c165a48ee1595e89e5802585e29
narimiran <narimiran@disroot.org>
2019-11-13 10:59:55 +0100

    nim: day13 - fix wrong `mod` and use IntSet

--- nim/day13.nim
+++ nim/day13.nim
@@ -1,6 +1,6 @@
-import strutils, sets, tables, sequtils, math
+import strutils, intsets, tables, sequtils, math
 
-proc makeFirewall(f: string): Table[int, HashSet[int]] =
+proc makeFirewall(f: string): Table[int, IntSet] =
   let instructions = readFile(f).splitLines()
 
   for line in instructions:
@@ -9,27 +9,27 @@ proc makeFirewall(f: string): Table[int, HashSet[int]] =
       depth = numbers[0]
       height = numbers[1]
     if not result.hasKey(height):
-      result[height] = initHashSet[int]()
+      result[height] = initIntSet()
     result[height].incl(depth)
 
-const firewall = makeFirewall("./inputs/13.txt")
+let firewall = makeFirewall("./inputs/13.txt")
 
 
 func isCaught(depth, height: int, delay = 0): bool =
   (depth + delay) mod (2 * (height - 1)) == 0
 
-func calculateSeverity(): int =
+func calculateSeverity(firewall: Table[int, IntSet]): int =
   for height in firewall.keys:
     for depth in firewall[height]:
       if isCaught(depth, height):
         result += depth * height
 
-func oneRemains(height: int, depths: HashSet[int]): bool =
+func oneRemains(height: int, depths: IntSet): bool =
   depths.card == height - 2
 
 
 type
-  Group = tuple[height: int, depths: HashSet[int]]
+  Group = tuple[height: int, depths: IntSet]
   Groups = seq[Group]
 var
   calcGroup: Groups = @[]
@@ -41,18 +41,23 @@ for h in firewall.keys:
   else:
     controlGroup.add((h, firewall[h]))
 
+func pymod(a, b: int): int =
+  let m = -a mod b
+  if m == 0: 0 else: m+b
 
-func findAllowedDelay(height: int, depths: HashSet[int]): tuple[delay, period: int] =
+func findAllowedDelay(height: int, depths: IntSet): tuple[delay, period: int] =
   let period = 2 * (height - 1)
   var
-    potential = initHashSet[int]()
-    forbidden = initHashSet[int]()
+    potential = initIntSet()
+    forbidden = initIntSet()
 
   for i in countup(0, period-1, 2): potential.incl(i)
-  for depth in depths: forbidden.incl(-depth mod period + period)
+  for depth in depths: forbidden.incl(pymod(depth, period))
 
   var allowed = potential - forbidden
-  result = (allowed.pop(), period)
+  var e: int # poor man's pop
+  for i in allowed: e = i; break
+  result = (e, period)
 
 
 func findDelayParams(walls: Groups): tuple[delay, step: int] =
@@ -65,15 +70,17 @@ func findDelayParams(walls: Groups): tuple[delay, step: int] =
     periods.add(period)
 
   let commonMulti = periods.foldl(lcm(a, b))
-  var allowedDelays = initHashSet[int]()
+  var allowedDelays = initIntSet()
   for i in 0 ..< commonMulti: allowedDelays.incl(i)
 
-  for dp in zip(delays, periods):
-    var s = initHashSet[int]()
-    for i in countup(0, commonMulti-1, dp.b): s.incl(dp.a + i)
+  for (d, p) in zip(delays, periods):
+    var s = initIntSet()
+    for i in countup(0, commonMulti-1, p): s.incl(d + i)
     allowedDelays = allowedDelays * s
 
-  result = (allowedDelays.pop(), commonMulti)
+  var e: int # poor man's pop
+  for i in allowedDelays: e = i; break
+  result = (e, commonMulti)
 
 
 func isCaught(hd: Group, delay: int): bool =
@@ -86,5 +93,5 @@ var (delay, step) = calcGroup.findDelayParams()
 while controlGroup.anyIt(it.isCaught(delay)):
   delay += step
 
-echo calculateSeverity()
+echo firewall.calculateSeverity()
 echo delay
